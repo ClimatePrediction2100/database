22ce05342f1e3f8b83cc269d1fd230f807806e4e
Jinho Heo <uj07077@gmail.com>
2024-05-30 13:39:27 +0900

    Refactor: file path

--- config.py
+++ config.py
@@ -5,14 +5,18 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 SSP_SCENARIOS = ["119", "126", "245", "370", "434", "460", "534", "585"]
 
-DATA_PATH = "climate_data"
+DATA_PATH = "data"
+
+RECORD_PATH = os.path.join(DATA_PATH, "recorded", "Land_and_Ocean_LatLong1.nc")
+RECORD_AVG_PATH = os.path.join(DATA_PATH, "recorded", "Land_and_Ocean_LatLong1_Average.csv")
+
 PREDICT_PATH_MAP = {
-    ssp: os.path.join(DATA_PATH, f"SSP{ssp}_predictions_convert.nc")
+    ssp: os.path.join(DATA_PATH, "predicted", "netCDF", f"SSP{ssp}_predictions.nc")
     for ssp in SSP_SCENARIOS
 }
 
-CONTINENT_PATH_MAP = {
-    ssp: os.path.join(DATA_PATH, f"SSP{ssp}_continent_average.csv")
+PREDICT_AVG_PATH_MAP = {
+    ssp: os.path.join(DATA_PATH, "predicted", "csv", f"SSP{ssp}_predictions_average.csv")
     for ssp in SSP_SCENARIOS
 }
 
