23ae2eb2de9a6593656e01ae56eb5c0eeb9b7069
Jinho Heo <uj07077@gmail.com>
2024-05-30 13:39:34 +0900

    Feat: data loading

---
+++ data/load_data.py
@@ -0,0 +1,35 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+
+import config
+
+recorded = xr.open_dataset(config.RECORD_PATH)
+recorded_avg = pd.read_csv(config.RECORD_AVG_PATH)
+predicted = {ssp: xr.open_dataset(path) for ssp, path in config.PREDICT_PATH_MAP.items()}
+predicted_avg = {ssp: pd.read_csv(path) for ssp, path in config.CONTINENT_PATH_MAP.items()}
+
+
+def getCoordData(lat_idx, lon_idx, season, ssp):
+    from_recorded = recorded['temperature'][0:1980].isel(latitude=lat_idx, longitude=lon_idx).values
+    from_predicted = predicted[ssp]['temperature'].isel(latitude=lat_idx, longitude=lon_idx).values
+    combined_temperature_list = from_recorded + from_predicted
+    
+    if season:
+        result = combined_temperature_list[config.SEASONS[season]::12]
+    else:
+        result = combined_temperature_list
+    
+    return result
+
+def getContinentData(continent, season, ssp):
+    from_recorded = recorded_avg[continent].tolist()[0:1980]
+    from_predicted = predicted_avg[ssp][continent].tolist()
+    combined_temperature_list = from_recorded + from_predicted
+    
+    if season:
+        result = combined_temperature_list[config.SEASONS[season]::12]
+    else:
+        result = combined_temperature_list
+    
+    return result
