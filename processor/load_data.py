feat: Fix to compute anual average correctly

processor/load_data.py
@@ -10,6 +10,13 @@ predicted = {ssp: xr.open_dataset(path) for ssp, path in config.PREDICT_PATH_MAP
 predicted_avg = {ssp: pd.read_csv(path) for ssp, path in config.PREDICT_AVG_PATH_MAP.items()}
 
 
+def compute_annual_average(monthly_data):
+    num_complete_years = len(monthly_data) // 12
+    valid_length = num_complete_years * 12
+    reshaped_data = np.reshape(monthly_data[:valid_length], (num_complete_years, 12))
+    annual_average = np.mean(reshaped_data, axis=1)
+    return annual_average
+
 def get_coord_data(lat_idx, lon_idx, season, ssp):
     from_recorded = recorded['temperature'][0:1980].isel(latitude=lat_idx, longitude=lon_idx).values
     from_predicted = predicted[ssp]['temperature'].isel(latitude=lat_idx, longitude=lon_idx).values
@@ -18,7 +25,7 @@ def get_coord_data(lat_idx, lon_idx, season, ssp):
     if season:
         result = combined_temperature_list[config.SEASONS[season]::12]
     else:
-        result = combined_temperature_list
+        result = compute_annual_average(combined_temperature_list)
     
     return result
 
@@ -30,6 +37,6 @@ def get_continent_data(continent, season, ssp):
     if season:
         result = combined_temperature_list[config.SEASONS[season]::12]
     else:
-        result = combined_temperature_list
+        result = result = compute_annual_average(combined_temperature_list)
     
     return result