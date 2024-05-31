e88d30de99f70c97a0ea9569d3a20f718362d328
reudekx <dohan@DESKTOP-NJJSHDT>
2024-05-31 14:59:55 +0900

    feat: Change urls

--- processor/load_data.py
+++ processor/load_data.py
@@ -17,7 +17,7 @@ def compute_annual_average(monthly_data):
     annual_average = np.mean(reshaped_data, axis=1)
     return annual_average
 
-def get_coord_data(lat_idx, lon_idx, season, ssp):
+def get_coord_data(lat_idx, lon_idx, ssp, season):
     from_recorded = recorded['temperature'][0:1980].isel(latitude=lat_idx, longitude=lon_idx).values
     from_predicted = predicted[ssp]['temperature'].isel(latitude=lat_idx, longitude=lon_idx).values
     combined_temperature_list = np.concatenate((from_recorded, from_predicted))
@@ -29,7 +29,7 @@ def get_coord_data(lat_idx, lon_idx, season, ssp):
     
     return result
 
-def get_continent_data(continent, season, ssp):
+def get_continent_data(continent, ssp, season):
     from_recorded = recorded_avg[continent].tolist()[0:1980]
     from_predicted = predicted_avg[ssp][continent].tolist()
     combined_temperature_list = np.concatenate((from_recorded, from_predicted))
@@ -39,4 +39,14 @@ def get_continent_data(continent, season, ssp):
     else:
         result = result = compute_annual_average(combined_temperature_list)
     
+    return result
+
+def get_data(continent, latitude, longitude, ssp, season):
+    if continent:
+        result = get_continent_data(continent, ssp, season)
+    else:
+        lat_idx = (latitude + 90) % 180
+        lon_idx = (longitude + 180) % 360
+        result = get_coord_data(lat_idx, lon_idx, ssp, season)
+
     return result
--- server.py
+++ server.py
@@ -4,32 +4,23 @@ import processor.convert_data as dataconverter
 
 app = Flask(__name__)
 
-@app.route('/coord', methods=['GET'])
-def get_coord_data():
-    latitude = int(request.args.get('latitude'))
-    longitude = int(request.args.get('longitude'))
+@app.route('/data', methods=['GET'])
+def get_data():
+    continent = request.args.get('continent', None)
 
-    lat_idx = (latitude + 90) % 180
-    lon_idx = (longitude + 180) % 360
-    
+    latitude = int(request.args.get('latitude', 0))
+    longitude = int(request.args.get('longitude', 0))
+
     season = request.args.get('season', default=None)
     ssp = request.args.get('ssp')
-    
-    result = dataloader.get_coord_data(lat_idx, lon_idx, season, ssp)
-    return jsonify(result.tolist())
 
-@app.route('/continent', methods=['GET'])
-def get_continent_data():
-    continent = request.args.get('continent')
-    season = request.args.get('season', default=None)
-    ssp = request.args.get('ssp')
-    
-    result = dataloader.get_continent_data(continent, season, ssp)
-    return jsonify(result.tolist())
+    data = dataloader.get_data(continent, latitude, longitude, ssp, season)
+
+    return jsonify(data.tolist())
 
 is_data_converted = False
 
-@app.route('/convert', methods=['GET'])
+@app.route('/data/convert', methods=['GET'])
 def convert_data():
     if not is_data_converted:
         dataconverter.save_average_data()
